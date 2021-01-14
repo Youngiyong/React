b2ae3cd72385aa7fb1cf832826289e56177cc59b
giyong <youn9354@naver.com>
2021-01-15 06:54:18 +0900

    second commit

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,15 @@
-
+/* eslint-disable */
 import React, {useState} from 'react';
 import { Navbar, Nav, NavDropdown,Button, Jumbotron} from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import Detail from './Detail.js';
+import { Link, Route, Switch } from 'react-router-dom';
+
 function App() {
 
   let [shoes, shoes변경] = useState(Data);
+
 
   return (
     <div className="App">
@@ -15,8 +19,8 @@ function App() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+      <Nav.Link><Link to="/">Home</Link></Nav.Link>
+      <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -27,7 +31,9 @@ function App() {
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-<Jumbotron className="background">
+<Switch>
+<Route exact path="/">
+    <Jumbotron className="background">
   <h1>20% Season Off</h1>
   <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
@@ -36,32 +42,41 @@ function App() {
   <p>
     <Button variant="primary">Learn more</Button>
   </p>
-</Jumbotron>
-
-
+    </Jumbotron>
 
      <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>{ shoes[0].title }</h4>
-            <p>{ shoes[0].content }</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-            <h4>{ shoes[1].title }</h4>
-            <p>{ shoes[1].content }</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-            <h4>{ shoes[2].title }</h4>
-            <p>{ shoes[2].content }</p>
-          </div>
+          {
+              shoes.map((a, idx)=>{
+                return <Card shoes={shoes[idx]} idx={idx}></Card>
+              })
+          }
         </div>
       </div>
+</Route>
+    <Route path="/detail/:id">
+        <Detail shoes={shoes} />
+    </Route>
 
+
+        <Route path="/:id">
+            <div>아무거나적었을때 이거 보여주셈</div>
+        </Route>
+    </Switch>
     </div>
   );
+}
+
+function Card(props){
+  return(
+      <div>
+          <div className="col-md-4">
+            <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg' } width="100%" />
+            <h4>{ props.shoes.title }</h4>
+            <p>{ props.shoes.content } & { props.shoes.price }</p>
+          </div>
+      </div>
+  )
 }
 
 export default App;
