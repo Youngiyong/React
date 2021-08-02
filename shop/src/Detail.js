d3405cee3fa540fe5aa55d3967ef223412cdcdde
giyong <youn9354@naver.com>
2021-08-03 00:57:53 +0900

    backup commit

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,6 +1,15 @@
 import React, {useState} from "react";
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss'
+let 박스 = styled.div`
+   padding : 20px;
+`;
 
+let 제목 = styled.h4`
+   font-size : 25px;
+   color : ${ props => props.색상 }
+`;
 function Detail(props){
 
     let { id } = useParams();
@@ -8,6 +17,9 @@ function Detail(props){
 
     return(
          <div className="container">
+             <박스>
+                <제목 className="red">상세페이지</제목>
+             </박스>
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
